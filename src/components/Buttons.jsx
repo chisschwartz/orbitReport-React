254a4ab61eb238e769429d5a96bfbcd6babca1d7
chisschwartz <christopherschwartz559@gmail.com>
2024-09-10 16:30:32 -0500

    all tests passing

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,32 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+import satData from "./satData";
+import React from "react";
+import './styling.css';
+
+// class Buttons extends React.Component{
+// render() {
+//   const { filterByType , setSat , displaySats } = this.props; 
+  
+function Buttons ({filterByType , setSat , displaySats}) {
+
+  const orbitClick = () => {
+    setSat(satData);
+  };
+
+//const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div id="flex-container">
+    {displaySats.map((sat , id) => {
+      return (
+      <button onClick = {() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
+      );
+      })}
+      <button onClick = {orbitClick}> All Orbits </button>
     </div>
-  );
+);
 };
+
+
 
 export default Buttons;
